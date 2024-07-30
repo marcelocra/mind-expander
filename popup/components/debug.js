369abacb0e88a11f0b6815ae8665f080c081e618
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-07-30 17:41:56 -0300

    feat: start debug component

---
+++ popup/components/debug.js
@@ -0,0 +1,11 @@
+/**
+ * A component to help debugging the application.
+ */
+
+import { html } from "../deps.js";
+
+const DEBUG = true;
+
+export function Debug() {
+  return html` <div></div> `;
+}
