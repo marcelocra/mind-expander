5ed3db7bab45e21a37de96b1c772769006bba136
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-07-30 17:04:55 -0300

    feat: get popup to work with preact!

--- popup/index.js
+++ popup/index.js
@@ -1,24 +1,21 @@
-// import { render , useReducer, html } from './deps.js';
-
-import { h, render, htm } from "./deps.js";
+import { h, render, htm, useReducer, useEffect } from "./deps.js";
 
 // Initialize htm with Preact
 const html = htm.bind(h);
 
 function App(props) {
-  return html`<h1>Hello ${props.name}!</h1>`;
+  const [count, add] = useReducer((a, b) => a + b, 0);
+
+  useEffect(() => {
+    console.log("Count changed to", count);
+  }, [count]);
+
+  return html`
+    <button onClick=${() => add(-1)}>Decrement</button>
+    <input readonly size="4" value=${count + 1} />
+    <input readonly size="4" value=${count} />
+    <button onClick=${() => add(1)}>Increment</button>
+  `;
 }
 
 render(html`<${App} name="World" />`, document.body);
-
-// export function App() {
-//   const [count, add] = useReducer((a, b) => a + b, 0);
-
-//   return html`
-//     <button onClick=${() => add(-1)}>Decrement</button>
-//     <input readonly size="4" value=${count} />
-//     <button onClick=${() => add(1)}>Increment</button>
-//   `;
-// }
-
-// render(html`<${App} />`, document.body);
