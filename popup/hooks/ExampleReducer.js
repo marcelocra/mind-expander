7601afe83edbf35b43a447d3ead238c2c86458a7
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-08-01 10:38:35 -0300

    fix: move ExampleReducer to a separate file

---
+++ popup/hooks/ExampleReducer.js
@@ -0,0 +1,21 @@
+import { html, useReducer, useEffect } from "../deps/preact.js";
+
+/** @param {{name: string}} props */
+function ExampleReducer({ name }) {
+  // @ts-ignore
+  const [count, add] = useReducer((a, b) => a + b, 0);
+
+  useEffect(() => {
+    console.log("Count changed to:", count);
+  }, [count]);
+
+  return html`
+    <div class="flex flex-col gap-3 justify-around min-h-screen bg-slate-100 p-10">
+      <h1 class="text-center text-lg font-bold">${name}</h1>
+      <button class="border rounded shadow-xl p-3 hover:animate-pulse" onClick=${() => add(-1)}>Decrement</button>
+      <input class="text-center bg-transparent" readonly value=${count + 1} />
+      <input class="text-center bg-transparent" readonly value=${count} />
+      <button class="border rounded shadow-xl p-3 hover:animate-pulse" onClick=${() => add(1)}>Increment</button>
+    </div>
+  `;
+}
