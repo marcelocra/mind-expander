72766f6ce0b14f827b15e0c6edf240aa364a64ea
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-08-05 21:37:38 -0300

    feat: finish working version of preact+twind settings

--- deps/twind.js
+++ deps/twind.js
@@ -1,10 +1,8 @@
-import "../vendored/twind-ext-forms-line_clamp-typography.js";
-
-twind.install({
-  presets: [
-    twind.presetExt(),
-    twind.presetLineClamp(),
-    twind.presetTailwindForms(),
-    twind.presetTypography(),
-  ],
-});
+// twind.install({
+//   presets: [
+//     twind.presetExt(),
+//     twind.presetLineClamp(),
+//     twind.presetTailwindForms(),
+//     twind.presetTypography(),
+//   ],
+// });
