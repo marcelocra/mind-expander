ef0dc3d3e25edbe92717c7b9405a0842bc1f0de2
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-08-07 21:04:17 -0300

    feat: add daisyui

--- src/popup-vue/tailwind.config.js
+++ src/popup-vue/tailwind.config.js
@@ -1,8 +1,10 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/popup-vite/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
