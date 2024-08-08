58c43541daafbe1b949c133f4e1dcc1a5fc438f4
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-08-07 21:20:44 -0300

    feat: update vue example

--- src/popup-vue/index.js
+++ src/popup-vue/index.js
@@ -8,11 +8,13 @@ const MyComponent = {
     const count = ref(0);
     const buttonClass = "border rounded shadow-xl p-3 w-32";
 
+    function increment() {
+      count.value++;
+    }
+
     return () => html`
       <div class="flex flex-col items-center justify-center gap-3">
-        <button class="${buttonClass}" onclick=${() => count.value++}>
-          Increment
-        </button>
+        <button class="${buttonClass}" onClick=${increment}>Increment</button>
         <p>${count.value}</p>
         <button class="${buttonClass}" onclick=${() => count.value--}>
           Decrement
@@ -23,27 +25,3 @@ const MyComponent = {
 };
 
 createApp(MyComponent).mount("#app");
-
-/*
-<script setup>
-import { ref, onMounted } from 'vue'
-
-// reactive state
-const count = ref(0)
-
-// functions that mutate state and trigger updates
-function increment() {
-  count.value++
-}
-
-// lifecycle hooks
-onMounted(() => {
-  console.log(`The initial count is ${count.value}.`)
-})
-</script>
-
-<template>
-  <button @click="increment">Count is: {{ count }}</button>
-</template>
-
-*/
--- src/popup-vue/tailwind.config.js
+++ src/popup-vue/tailwind.config.js
@@ -2,7 +2,7 @@ import { plugins, theme } from "../libs/common-tailwind.config";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/popup-vite/**/*.{html,js}"],
+  content: ["./src/popup-vue/**/*.{html,js}"],
   theme,
   plugins,
 };
