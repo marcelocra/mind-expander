f0d34109f4c80de9c32e3f6d51bb8109595905e4
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-08-08 02:03:41 -0300

    feat: get prettier tw to work

--- src/popup/components/App.js
+++ src/popup/components/App.js
@@ -1,18 +1,25 @@
 import { t } from "../../libs/i18n.js";
 import { html } from "../../vendored/htm-preact.js";
 
-export function App() {
-  const buttonClass =
-    "border border-gray-300 w-full rounded px-3 py-1 shadow-xl text-sm text-nowrap hover:bg-gray-100";
+/** @param {any} props */
+function Button({ onClick, children }) {
+  return html`
+    <button
+      class="w-32 rounded border px-3 py-1 text-sm shadow-xl hover:bg-gray-100"
+      onClick=${onClick}
+    >
+      ${children}
+    </button>
+  `;
+}
 
+export function App() {
   return html`
     <div class="flex flex-col items-center justify-center gap-3">
-      <button class=${buttonClass} onClick=${() => alert("save")}>
-        ${t.popupIcon.savePage}
-      </button>
-      <button class=${buttonClass} onClick=${() => alert("summarize")}>
+      <${Button} onClick=${() => alert("save")}>${t.popupIcon.savePage}<//>
+      <${Button} onClick=${() => alert("summarize")}>
         ${t.popupIcon.summarize}
-      </button>
+      <//>
     </div>
   `;
 }
